dde9c6fcb71bf065a0c48ee3e8443a7bfb62cbb4
Zach Fuller <zach.fuller1222@gmail.com>
2024-03-17 14:11:04 -0700

    creating new image with half-sized dimensions. wip: copying over image contents to resized img

--- resizer.go
+++ resizer.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/jpeg"
 	"log/slog"
 	"os"
 )
 
+func copyImage(img *image.Image) error {
+	// create destination file
+	dstFile, err := os.Create("copy-test.jpeg")
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	// create new image
+	maxX := (*img).Bounds().Dx() / 2
+	maxY := (*img).Bounds().Dy() / 2
+	// newImg := image.NewRGBA((*img).Bounds())
+	newImg := image.NewRGBA(image.Rect(0, 0, maxX, maxY))
+	fmt.Printf("New image bounds: %v\n", newImg.Bounds())
+
+	err = jpeg.Encode(dstFile, newImg, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // Remove the unused function createIconSize
 func createIconSize(srcPath string) error {
-	fmt.Printf("Creating icon size\n")
+	fmt.Printf("Creating icon size for %s\n", srcPath)
 	srcFile, err := os.Open(srcPath)
 	if err != nil {
 		return err
@@ -23,7 +46,11 @@ func createIconSize(srcPath string) error {
 	}
 
 	fmt.Printf("Image bounds: %v\n", img.Bounds())
-	fmt.Printf("Image color model: %v\n", img.ColorModel())
+	fmt.Printf("Image color model: %s\n", img.ColorModel())
 	fmt.Printf("Image width: %d, height: %d\n", img.Bounds().Dx(), img.Bounds().Dy())
+	err = copyImage(&img)
+	if err != nil {
+		return err
+	}
 	return nil
 }
