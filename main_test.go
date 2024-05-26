71eeda2c2a3083ead68127ed65ef7174a363f2fb
Zach Fuller <zach.fuller1222@gmail.com>
2024-05-26 13:19:47 -0700

    added additional test

--- main_test.go
+++ main_test.go
@@ -10,6 +10,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestLoadConfig(t *testing.T) {
+	loadConfig()
+	if assert.NotEmpty(t, conf) {
+		assert.Equal(t, "/opt/picloud/uploads/", conf.FilePrefix)
+	}
+}
+
+// func TestLoadFileMetadata(t *testing.T) {
+// 	// TODO: inject mock data to load for this test and assert mocked data is returned
+// 	expected := `{"files":[{"name":"baxter.jpg","tags":[],"link":"baxter.jpg"}}`
+// 	uploadedFiles = loadFileMetadata()
+// 	if assert.NotEmpty(t, conf) {
+// 		assert.Equal(t, "/opt/picloud/uploads/", conf.FilePrefix)
+// 	}
+// }
+
 func TestListFiles(t *testing.T) {
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/files", nil)
