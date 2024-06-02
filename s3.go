6451d3adafccbe95c601364aaf49f991ccfed86e
Zach Fuller <zach.fuller1222@gmail.com>
2024-06-02 16:29:05 -0700

    updated error handling + loading bucket name from env var

--- s3.go
+++ s3.go
@@ -3,26 +3,27 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func uploadFileToS3(metadata *FileMetadata, fileContent []byte) (string, error) {
+func uploadFileToS3(metadata *FileMetadata, fileContent []byte) error {
+	bucket := os.Getenv("S3_BUCKET")
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		return "", err
+		return err
 	}
 	client := s3.NewFromConfig(cfg)
 	_, err = client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String("my-bucket"), // TODO: Load name of bucket from environment variable
+		Bucket: aws.String(bucket),
 		Key:    aws.String(metadata.Name),
 		Body:   bytes.NewReader(fileContent),
 	})
 	if err != nil {
-		return "", err
+		return err
 	}
-	return fmt.Sprintf("https://my-bucket.s3.amazonaws.com/%s", metadata.Name), nil
+	return nil
 }
